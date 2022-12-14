b50a3fb730c0b4356f9160176f6c14887dd6c52a
Robert Ancell <robert.ancell@gmail.com>
2022-12-15 11:36:46 +1300

    Implement _NET_WM_SYNC_REQUEST

--- src/x11/ut-x11-client-test.c
+++ src/x11/ut-x11-client-test.c
@@ -9,11 +9,17 @@ static size_t pixmap_height = 0;
 static UtObject *client = NULL;
 static uint32_t wm_protocols_atom = 0;
 static uint32_t wm_delete_window_atom = 0;
+static uint32_t wm_sync_request_atom = 0;
+static uint32_t wm_sync_request_counter_atom = 0;
 static uint32_t segment = 0;
 static UtObject *buffer = NULL;
 static uint32_t window = 0;
 static uint32_t pixmap = 0;
 static uint32_t gc = 0;
+static uint32_t sync_counter = 0;
+static uint32_t extended_sync_counter = 0;
+static bool sync_counter_value_is_extended = false;
+static int64_t sync_counter_value = 0;
 
 static void enter_notify_cb(void *user_data, uint32_t window, int16_t x,
                             int16_t y) {
@@ -63,8 +69,17 @@ static void client_message_cb(void *user_data, uint32_t window, uint32_t type,
   printf("ClientMessage\n");
 
   if (type == wm_protocols_atom) {
-    if (ut_uint32_list_get_element(data, 0) == wm_delete_window_atom) {
+    uint32_t protocol = ut_uint32_list_get_element(data, 0);
+    if (protocol == wm_delete_window_atom) {
       ut_event_loop_return(NULL);
+    } else if (protocol == wm_sync_request_atom) {
+      uint64_t counter_value_low = ut_uint32_list_get_element(data, 2);
+      uint64_t counter_value_high = ut_uint32_list_get_element(data, 3);
+      sync_counter_value_is_extended = ut_uint32_list_get_element(data, 4) != 0;
+      sync_counter_value =
+          (int64_t)(counter_value_high << 32 | counter_value_low);
+      printf("%s %d %li\n", ut_object_to_string(data),
+             sync_counter_value_is_extended, sync_counter_value);
     }
   }
 }
@@ -110,6 +125,8 @@ static void expose_cb(void *user_data, uint32_t window, uint16_t x, uint16_t y,
   printf("Expose\n");
   ut_x11_client_copy_area(client, pixmap, window, gc, x, y, x, y, width,
                           height);
+
+  ut_x11_client_set_counter(client, extended_sync_counter, sync_counter_value);
 }
 
 static UtX11EventCallbacks event_callbacks = {
@@ -150,11 +167,32 @@ static void wm_delete_window_atom_cb(void *user_data, uint32_t atom,
                                      UtObject *error) {
   assert(error == NULL);
   wm_delete_window_atom = atom;
+}
 
+static void wm_sync_request_atom_cb(void *user_data, uint32_t atom,
+                                    UtObject *error) {
+  assert(error == NULL);
+  wm_sync_request_atom = atom;
+}
+
+static void wm_sync_request_counter_atom_cb(void *user_data, uint32_t atom,
+                                            UtObject *error) {
+  assert(error == NULL);
+  wm_sync_request_counter_atom = atom;
+
+  // We support window deletion requests and paint synchronization.
   UtObjectRef protocols = ut_uint32_list_new();
   ut_uint32_list_append(protocols, wm_delete_window_atom);
+  ut_uint32_list_append(protocols, wm_sync_request_atom);
   ut_x11_client_change_property_uint32(client, window, wm_protocols_atom,
                                        UT_X11_ATOM, protocols);
+
+  UtObjectRef counter_value = ut_uint32_list_new();
+  ut_uint32_list_append(counter_value, sync_counter);
+  ut_uint32_list_append(counter_value, extended_sync_counter);
+  ut_x11_client_change_property_uint32(client, window,
+                                       wm_sync_request_counter_atom,
+                                       UT_X11_CARDINAL, counter_value);
 }
 
 static void connect_cb(void *user_data, UtObject *error) {
@@ -172,6 +210,10 @@ static void connect_cb(void *user_data, UtObject *error) {
                             NULL, NULL);
   ut_x11_client_intern_atom(client, "WM_DELETE_WINDOW", false,
                             wm_delete_window_atom_cb, NULL, NULL);
+  ut_x11_client_intern_atom(client, "_NET_WM_SYNC_REQUEST", false,
+                            wm_sync_request_atom_cb, NULL, NULL);
+  ut_x11_client_intern_atom(client, "_NET_WM_SYNC_REQUEST_COUNTER", false,
+                            wm_sync_request_counter_atom_cb, NULL, NULL);
 
   window = ut_x11_client_create_window(
       client, 0, 0, 640, 480,
@@ -180,6 +222,8 @@ static void connect_cb(void *user_data, UtObject *error) {
           UT_X11_EVENT_ENTER_WINDOW | UT_X11_EVENT_LEAVE_WINDOW |
           UT_X11_EVENT_POINTER_MOTION | UT_X11_EVENT_STRUCTURE_NOTIFY |
           UT_X11_EVENT_EXPOSURE | UT_X11_EVENT_FOCUS_CHANGE);
+  sync_counter = ut_x11_client_create_counter(client, 0);
+  extended_sync_counter = ut_x11_client_create_counter(client, 0);
 
   ut_x11_client_change_property_string(client, window, UT_X11_WM_NAME, "UT");
 
