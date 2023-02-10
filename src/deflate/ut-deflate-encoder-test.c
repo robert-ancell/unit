b016afc444769c8c21c92b7da7f459837b5b8e1d
Robert Ancell <robert.ancell@gmail.com>
2023-02-10 13:05:43 +1300

    Add helper method for encoding utf8 in test

--- src/deflate/ut-deflate-encoder-test.c
+++ src/deflate/ut-deflate-encoder-test.c
@@ -1,5 +1,10 @@
 #include "ut.h"
 
+static UtObject *get_utf8_data(const char *value) {
+  UtObjectRef string = ut_string_new(value);
+  return ut_string_get_utf8(string);
+}
+
 int main(int argc, char **argv) {
   UtObjectRef empty_data = ut_uint8_list_new();
   UtObjectRef empty_data_stream = ut_list_input_stream_new(empty_data);
@@ -25,8 +30,7 @@ int main(int argc, char **argv) {
   ut_assert_uint8_list_equal_hex(double_result, "63600000");
 
   // Encodes as "!" (no repetition)
-  UtObjectRef single_char_string = ut_string_new("!");
-  UtObjectRef single_char_data = ut_string_get_utf8(single_char_string);
+  UtObjectRef single_char_data = get_utf8_data("!");
   UtObjectRef single_char_data_stream =
       ut_list_input_stream_new(single_char_data);
   UtObjectRef single_char_encoder =
@@ -37,8 +41,7 @@ int main(int argc, char **argv) {
   ut_assert_uint8_list_equal_hex(single_char_result, "530400");
 
   // Encodes as "hello" (no repetition)
-  UtObjectRef hello_string = ut_string_new("hello");
-  UtObjectRef hello_data = ut_string_get_utf8(hello_string);
+  UtObjectRef hello_data = get_utf8_data("hello");
   UtObjectRef hello_data_stream = ut_list_input_stream_new(hello_data);
   UtObjectRef hello_encoder = ut_deflate_encoder_new(hello_data_stream);
   UtObjectRef hello_result = ut_input_stream_read_sync(hello_encoder);
@@ -46,8 +49,7 @@ int main(int argc, char **argv) {
   ut_assert_uint8_list_equal_hex(hello_result, "cb48cdc9c90700");
 
   // Encodes as "hello " rep(6,11)
-  UtObjectRef hello3_string = ut_string_new("hello hello hello");
-  UtObjectRef hello3_data = ut_string_get_utf8(hello3_string);
+  UtObjectRef hello3_data = get_utf8_data("hello hello hello");
   UtObjectRef hello3_data_stream = ut_list_input_stream_new(hello3_data);
   UtObjectRef hello3_encoder = ut_deflate_encoder_new(hello3_data_stream);
   UtObjectRef hello3_result = ut_input_stream_read_sync(hello3_encoder);
@@ -55,8 +57,7 @@ int main(int argc, char **argv) {
   ut_assert_uint8_list_equal_hex(hello3_result, "cb48cdc9c957402201");
 
   // Encodes as "our f" rep(5,4) "h" rep(10,7) "tain"
-  UtObjectRef repeat_phrase_string = ut_string_new("our four hour fountain");
-  UtObjectRef repeat_phrase_data = ut_string_get_utf8(repeat_phrase_string);
+  UtObjectRef repeat_phrase_data = get_utf8_data("our four hour fountain");
   UtObjectRef repeat_phrase_data_stream =
       ut_list_input_stream_new(repeat_phrase_data);
   UtObjectRef repeat_phrase_encoder =
