73f388785b83e946ef2182daa494615c562972ea
Robert Ancell <robert.ancell@gmail.com>
2023-07-01 23:02:00 +1200

    WIP

--- src/http/ut-http-client.c
+++ src/http/ut-http-client.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 
 #include "ut-http-message-decoder.h"
+#include "ut-http-message-encoder.h"
 #include "ut.h"
 
 // FIXME: Make a real object
@@ -11,8 +12,7 @@ typedef struct {
   UtObject *cancel;
   char *host;
   uint16_t port;
-  char *method;
-  char *path;
+  UtObject *message_encoder;
   UtHttpResponseCallback callback;
   void *callback_user_data;
   UtObject *callback_cancel;
@@ -26,10 +26,13 @@ HttpRequest *http_request_new(const char *host, uint16_t port,
                               void *callback_user_data,
                               UtObject *callback_cancel) {
   HttpRequest *request = malloc(sizeof(HttpRequest));
-  request->host = ut_cstring_new(host);
   request->port = port;
-  request->method = ut_cstring_new(method);
-  request->path = ut_cstring_new(path);
+  UtObjectRef headers = ut_list_new();
+  ut_list_append_take(headers, ut_http_header_new("Host", host));
+  UtObjectRef empty_body = ut_uint8_list_new();
+  UtObjectRef body = ut_list_input_stream_new(empty_body);
+  request->message_encoder =
+      ut_http_message_encoder_new_request(NULL, method, path, headers, body);
   request->cancel = ut_cancel_new();
   request->callback = callback;
   request->callback_user_data = callback_user_data;
@@ -43,9 +46,7 @@ HttpRequest *http_request_new(const char *host, uint16_t port,
 void http_request_free(HttpRequest *request) {
   ut_object_unref(request->tcp_socket);
   ut_object_unref(request->cancel);
-  free(request->host);
-  free(request->method);
-  free(request->path);
+  ut_object_unref(request->message_encoder);
   ut_object_unref(request->callback_cancel);
   ut_object_unref(request->message_decoder_input_stream);
   ut_object_unref(request->message_decoder);
@@ -92,16 +93,8 @@ static size_t read_cb(void *user_data, UtObject *data, bool complete) {
 static void connect_cb(void *user_data) {
   HttpRequest *request = user_data;
 
-  UtObjectRef header = ut_string_new(request->method);
-  ut_string_append(header, " ");
-  ut_string_append(header, request->path);
-  ut_string_append(header, " HTTP/1.1\r\n");
-  ut_string_append(header, "Host: ");
-  ut_string_append(header, request->host);
-  ut_string_append(header, "\r\n");
-  ut_string_append(header, "\r\n");
-  UtObjectRef utf8 = ut_string_get_utf8(header);
-  ut_output_stream_write(request->tcp_socket, utf8);
+  ut_http_message_encoder_encode(request->message_encoder);
+
   ut_http_message_decoder_read(request->message_decoder);
   ut_input_stream_read(request->tcp_socket, read_cb, request, request->cancel);
 }
