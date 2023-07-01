9ad4b8e94f7f98f054a991c4f0b8c7f42a68900d
Robert Ancell <robert.ancell@gmail.com>
2023-07-01 23:18:26 +1200

    Add http example

---
+++ examples/http.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include "ut.h"
+
+static size_t body_cb(void *user_data, UtObject *data, bool complete) {
+  UtObjectRef data_string = ut_string_new_from_utf8(data);
+  printf("%s", ut_string_get_text(data_string));
+
+  UtObjectRef return_code = ut_int32_new(0);
+  ut_event_loop_return(return_code);
+
+  return ut_list_get_length(data);
+}
+
+static void response_cb(void *user_data, UtObject *response) {
+  if (ut_http_response_get_status_code(response) != 200) {
+    printf("%s\n", ut_http_response_get_reason_phrase(response));
+    UtObjectRef return_code = ut_int32_new(1);
+    ut_event_loop_return(return_code);
+    return;
+  }
+
+  ut_input_stream_read_all(ut_http_response_get_body(response), body_cb, NULL,
+                           NULL);
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Usage: http [uri]\n");
+    return 1;
+  }
+  const char *uri = argv[1];
+
+  UtObjectRef client = ut_http_client_new();
+
+  ut_http_client_send_request(client, "GET", uri, response_cb, NULL, NULL);
+
+  UtObjectRef return_code = ut_event_loop_run();
+  return ut_int32_get_value(return_code);
+}
