84fb5b783590f21554ebbbe2cea66fab385d98bb
Robert Ancell <robert.ancell@gmail.com>
2023-09-02 19:10:57 +1200

    Test repeated fields

--- src/protobuf/ut-protobuf-decoder-test.c
+++ src/protobuf/ut-protobuf-decoder-test.c
@@ -41,6 +41,9 @@ static void test_uint32() {
   test_uint32_value(message, "088001", 128);
   test_uint32_value(message, "08ffffffff0f", UINT32_MAX);
 
+  // Second value is the one kept.
+  test_uint32_value(message, "08010802", 2);
+
   test_decode_error(message, "090000000000000000",
                     "Received I64 for non-I64 field");
   test_decode_error(message, "0a00", "Received LEN for non-LEN field");
@@ -77,6 +80,9 @@ static void test_int32() {
   test_int32_value(message, "088001", 128);
   test_int32_value(message, "08ffffffff07", INT32_MAX);
   test_int32_value(message, "0880808080f8ffffffff01", INT32_MIN);
+
+  // Second value is the one kept.
+  test_int32_value(message, "08010802", 2);
 
   test_decode_error(message, "090000000000000000",
                     "Received I64 for non-I64 field");
@@ -117,6 +123,9 @@ static void test_sint32() {
   test_sint32_value(message, "08feffffff0f", INT32_MAX);
   test_sint32_value(message, "08ffffffff0f", INT32_MIN);
 
+  // Second value is the one kept.
+  test_sint32_value(message, "08020804", 2);
+
   test_decode_error(message, "090000000000000000",
                     "Received I64 for non-I64 field");
   test_decode_error(message, "0a00", "Received LEN for non-LEN field");
@@ -150,6 +159,9 @@ static void test_fixed32() {
   test_fixed32_value(message, "0d01000000", 1);
   test_fixed32_value(message, "0d78563412", 0x12345678);
   test_fixed32_value(message, "0dffffffff", UINT32_MAX);
+
+  // Second value is the one kept.
+  test_fixed32_value(message, "0d010000000d02000000", 2);
 
   test_decode_error(message, "0800", "Received VARINT for non-VARINT field");
   test_decode_error(message, "090000000000000000",
@@ -189,6 +201,9 @@ static void test_sfixed32() {
   test_sfixed32_value(message, "0dffffff7f", INT32_MAX);
   test_sfixed32_value(message, "0d00000080", INT32_MIN);
 
+  // Second value is the one kept.
+  test_sfixed32_value(message, "0d010000000d02000000", 2);
+
   test_decode_error(message, "0800", "Received VARINT for non-VARINT field");
   test_decode_error(message, "090000000000000000",
                     "Received I64 for non-I64 field");
@@ -225,6 +240,9 @@ static void test_uint64() {
   test_uint64_value(message, "087f", 127);
   test_uint64_value(message, "088001", 128);
   test_uint64_value(message, "08ffffffffffffffffff01", UINT64_MAX);
+
+  // Second value is the one kept.
+  test_uint64_value(message, "08010802", 2);
 
   test_decode_error(message, "090000000000000000",
                     "Received I64 for non-I64 field");
@@ -263,6 +281,9 @@ static void test_int64() {
   test_int64_value(message, "08ffffffffffffffff7f", INT64_MAX);
   test_int64_value(message, "0880808080808080808001", INT64_MIN);
 
+  // Second value is the one kept.
+  test_int64_value(message, "08010802", 2);
+
   test_decode_error(message, "090000000000000000",
                     "Received I64 for non-I64 field");
   test_decode_error(message, "0a00", "Received LEN for non-LEN field");
@@ -300,6 +321,9 @@ static void test_sint64() {
   test_sint64_value(message, "08feffffffffffffffff01", INT64_MAX);
   test_sint64_value(message, "08ffffffffffffffffff01", INT64_MIN);
 
+  // Second value is the one kept.
+  test_sint64_value(message, "08020804", 2);
+
   test_decode_error(message, "090000000000000000",
                     "Received I64 for non-I64 field");
   test_decode_error(message, "0a00", "Received LEN for non-LEN field");
@@ -333,6 +357,9 @@ static void test_fixed64() {
   test_fixed64_value(message, "090100000000000000", 1);
   test_fixed64_value(message, "09f0debc9a78563412", 0x123456789abcdef0);
   test_fixed64_value(message, "09ffffffffffffffff", UINT64_MAX);
+
+  // Second value is the one kept.
+  test_fixed64_value(message, "090100000000000000090200000000000000", 2);
 
   test_decode_error(message, "0800", "Received VARINT for non-VARINT field");
   test_decode_error(message, "0a00", "Received LEN for non-LEN field");
@@ -375,6 +402,9 @@ static void test_sfixed64() {
   test_sfixed64_value(message, "09ffffffffffffff7f", INT64_MAX);
   test_sfixed64_value(message, "090000000000000080", INT64_MIN);
 
+  // Second value is the one kept.
+  test_sfixed64_value(message, "09010000000000000009ffffffffffffffff", -1);
+
   test_decode_error(message, "0800", "Received VARINT for non-VARINT field");
   test_decode_error(message, "0a00", "Received LEN for non-LEN field");
   test_decode_error(message, "0b00", "Unknown wire type");
@@ -416,6 +446,9 @@ static void test_bool() {
   test_bool_value(message, "0800", false);
   test_bool_value(message, "0801", true);
 
+  // Second value is the one kept.
+  test_bool_value(message, "08000801", true);
+
   test_decode_error(message, "090000000000000000",
                     "Received I64 for non-I64 field");
   test_decode_error(message, "0a00", "Received LEN for non-LEN field");
@@ -451,6 +484,9 @@ static void test_float() {
   test_float_value(message, "0d000080bf", -1);
   test_float_value(message, "0dd00f4940", 3.14159);
   test_float_value(message, "0dffff7f7f", FLT_MAX);
+
+  // Second value is the one kept.
+  test_float_value(message, "0d0000803f0d000080bf", -1);
 
   test_decode_error(message, "0800", "Received VARINT for non-VARINT field");
   test_decode_error(message, "090000000000000000",
@@ -488,6 +524,9 @@ static void test_double() {
   test_double_value(message, "09000000000000f0bf", -1);
   test_double_value(message, "096e861bf0f9210940", 3.14159);
   test_double_value(message, "09ffffffffffffef7f", DBL_MAX);
+
+  // Second value is the one kept.
+  test_double_value(message, "09000000000000f03f09000000000000f0bf", -1);
 
   test_decode_error(message, "0800", "Received VARINT for non-VARINT field");
   test_decode_error(message, "0a00", "Received LEN for non-LEN field");
@@ -528,6 +567,9 @@ static void test_string() {
   test_string_value(message, "0a0c48656c6c6f20576f726c6421", "Hello World!");
   test_string_value(message, "0a0e24c2a2e282acf09090b7f09f9880", "$¢€𐐷😀");
 
+  // Second value is the one kept.
+  test_string_value(message, "0a01480a0157", "W");
+
   // FIXME: Invalid UTF-8.
 
   test_decode_error(message, "0800", "Received VARINT for non-VARINT field");
@@ -562,6 +604,9 @@ static void test_bytes() {
   test_bytes_value(message, "0a00", "");
   test_bytes_value(message, "0a01ff", "ff");
   test_bytes_value(message, "0a04deadbeef", "deadbeef");
+
+  // Second value is the one kept.
+  test_bytes_value(message, "0a01de0a01ad", "ad");
 
   test_decode_error(message, "0800", "Received VARINT for non-VARINT field");
   test_decode_error(message, "090000000000000000",
@@ -599,6 +644,9 @@ static void test_enum() {
   test_enum_value(message, "0801", "ONE");
   test_enum_value(message, "0802", "TWO");
   test_enum_value(message, "082a", "42");
+
+  // Second value is the one kept.
+  test_enum_value(message, "08010802", "TWO");
 
   test_decode_error(message, "090000000000000000",
                     "Received I64 for non-I64 field");
