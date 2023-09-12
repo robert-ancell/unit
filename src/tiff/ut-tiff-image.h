a1262864d69dd0141cc9563f8f7de99b8b73fbfd
Robert Ancell <robert.ancell@gmail.com>
2023-09-12 22:37:17 +1200

    Document more TIFF methods

--- src/tiff/ut-tiff-image.h
+++ src/tiff/ut-tiff-image.h
@@ -6,6 +6,11 @@
 
 #pragma once
 
+/// Creates a new TIFF image with dimensions [width] and [length].
+///
+/// !arg-type data UtUint8List
+/// !return-ref
+/// !return-type UtTiffReader UtTiffError
 UtObject *
 ut_tiff_image_new(uint32_t width, uint32_t length,
                   UtTiffPhotometricInterpretation photometric_interpretation,
@@ -13,12 +18,20 @@ ut_tiff_image_new(uint32_t width, uint32_t length,
                   uint16_t bits_per_sample, uint16_t samples_per_pixel,
                   UtObject *data);
 
+/// Creates a new [UtTiffImage] from [data].
+///
+/// !arg-type data UtUint8List
+/// !return-ref
+/// !return-type UtTiffReader UtTiffError
 UtObject *ut_tiff_image_new_from_data(UtObject *data);
 
+/// Returns the width of the image in pixels.
 uint32_t ut_tiff_image_get_width(UtObject *object);
 
+/// Returns the length of the image in pixels.
 uint32_t ut_tiff_image_get_length(UtObject *object);
 
+/// Returns the method used to convert the samples to color values.
 UtTiffPhotometricInterpretation
 ut_tiff_image_get_photometric_interpretation(UtObject *object);
 
@@ -29,14 +42,22 @@ uint16_t ut_tiff_image_get_bits_per_sample(UtObject *object);
 
 uint16_t ut_tiff_image_get_samples_per_pixel(UtObject *object);
 
+/// Returns the row stride of the image data.
 size_t ut_tiff_image_get_row_stride(UtObject *object);
 
 void ut_tiff_image_set_color_map(UtObject *object, UtObject *color_map);
 
 UtObject *ut_tiff_image_get_color_map(UtObject *object);
 
+/// Returns the image data.
+///
+/// !return-type UtUint8List
 UtObject *ut_tiff_image_get_data(UtObject *object);
 
+/// Returns the image data converted to RGBA form.
+///
+/// !return-ref
+/// !return-type UtUint8List
 UtObject *ut_tiff_image_to_rgba(UtObject *object);
 
 /// Returns [true] if [object] is a [UtTiffImage].
--- src/tiff/ut-tiff-reader.h
+++ src/tiff/ut-tiff-reader.h
@@ -4,46 +4,73 @@
 
 #pragma once
 
+/// Creates a new [UtTiffReader] to decode [data].
+///
+/// !arg-type data UtUint8List
+/// !return-ref
+/// !return-type UtTiffReader
 UtObject *ut_tiff_reader_new(UtObject *data);
 
+/// Returns the tags read.
+///
+/// !return-type UtObjectList
 UtObject *ut_tiff_reader_get_tags(UtObject *object);
 
+/// Returns the first tag that matches [id] or [NULL] if none.
+///
+/// !return-type UtTiffTag NULL
 UtObject *ut_tiff_reader_get_tag(UtObject *object, uint16_t id);
 
+/// Returns the width of the image in pixels.
 uint32_t ut_tiff_reader_get_image_width(UtObject *object);
 
+/// Returns the length of the image in pixels.
 uint32_t ut_tiff_reader_get_image_length(UtObject *object);
 
+/// Returns the number of bits per pixel sample.
 uint16_t ut_tiff_reader_get_bits_per_sample(UtObject *object);
 
+/// Returns the compression used in the image.
+/// See [UtTiffCompression] for standard values.
 uint16_t ut_tiff_reader_get_compression(UtObject *object);
 
+/// Returns the method used to convert the samples to color values.
+/// See [UtTiffPhotometricInterpretation] for standard values.
 uint16_t ut_tiff_reader_get_photometric_interpretation(UtObject *object);
 
 uint16_t ut_tiff_reader_get_thresholding(UtObject *object);
 
+/// Returns the width of a cell in the image.
 uint16_t ut_tiff_reader_get_cell_width(UtObject *object);
 
+/// Returns the length of a cell in the image.
 uint16_t ut_tiff_reader_get_cell_length(UtObject *object);
 
 uint16_t ut_tiff_reader_get_fill_order(UtObject *object);
 
+/// Returns the name of the document.
 char *ut_tiff_reader_get_document_name(UtObject *object);
 
+/// Returns the description of the image.
 char *ut_tiff_reader_get_image_description(UtObject *object);
 
+/// Returns the make of the device that generated the image.
 char *ut_tiff_reader_get_make(UtObject *object);
 
+/// Returns the model of the device that generated the image.
 char *ut_tiff_reader_get_model(UtObject *object);
 
+/// Returns the orientation of the image.
 uint16_t ut_tiff_reader_get_orientation(UtObject *object);
 
+/// Returns the number of samples in each pixel.
 uint16_t ut_tiff_reader_get_samples_per_pixel(UtObject *object);
 
 uint32_t ut_tiff_reader_get_rows_per_strip(UtObject *object);
 
 uint16_t ut_tiff_reader_get_planar_configuration(UtObject *object);
 
+/// Returns the name of the page.
 char *ut_tiff_reader_get_page_name(UtObject *object);
 
 uint16_t ut_tiff_reader_get_gray_response_unit(UtObject *object);
@@ -53,16 +80,21 @@ uint16_t ut_tiff_reader_get_resolution_unit(UtObject *object);
 uint16_t ut_tiff_reader_get_page_number(UtObject *object,
                                         uint16_t *total_pages);
 
+/// Returns the name of the software that generated the image.
 char *ut_tiff_reader_get_software(UtObject *object);
 
 char *ut_tiff_reader_get_date_time(UtObject *object);
 
+/// Returns the name of the arists who created the image.
 char *ut_tiff_reader_get_artist(UtObject *object);
 
+/// Returns the name of host computer that generated the image.
 char *ut_tiff_reader_get_host_computer(UtObject *object);
 
+/// Returns the width of a tile in the image.
 uint32_t ut_tiff_reader_get_tile_width(UtObject *object);
 
+/// Returns the length of a tile in the image.
 uint32_t ut_tiff_reader_get_tile_length(UtObject *object);
 
 uint16_t ut_tiff_reader_get_ink_set(UtObject *object);
@@ -71,10 +103,17 @@ char *ut_tiff_reader_get_ink_names(UtObject *object);
 
 char *ut_tiff_reader_get_target_printer(UtObject *object);
 
+/// Returns the copyright assigned to the image.
 char *ut_tiff_reader_get_copyright(UtObject *object);
 
+/// Returns a sub file at position [offset] in this file.
+///
+/// !return-type UtTiffReaer
 UtObject *ut_tiff_reader_get_subfile(UtObject *object, uint32_t offset);
 
+/// Returns the first error that occurred during decoding of [NULL] if no error.
+///
+/// !return-type UtTiffError NULL
 UtObject *ut_tiff_reader_get_error(UtObject *object);
 
 /// Returns [true] if [object] is a [UtTiffReader].
