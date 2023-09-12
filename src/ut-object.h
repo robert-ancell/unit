b3fa16fb4737278ee265c8dd602df22e34e9a873
Robert Ancell <robert.ancell@gmail.com>
2023-09-12 22:58:59 +1200

    Document UtObject

--- src/ut-object.h
+++ src/ut-object.h
@@ -29,29 +29,46 @@ UtObject *ut_object_new(size_t object_size, UtObjectInterface *functions);
 
 void *ut_object_get_interface(UtObject *object, void *interface_id);
 
+/// Returns the name of this type.
 const char *ut_object_get_type_name(UtObject *object);
 
+/// Generates a human readable string summary of this object.
 char *ut_object_to_string(UtObject *object);
 
+/// Returns true if this object is the same as [other].
+///
+/// !arg-type other UtObject
 bool ut_object_equal(UtObject *object, UtObject *other);
 
 int ut_object_get_hash(UtObject *object);
 
-// Returns NULL if [object] is NULL.
+/// Increase the reference count on this object.
+/// Returns [NULL] if [object] is [NULL].
 UtObject *ut_object_ref(UtObject *object);
 
-// Does nothing if [object] is NULL.
+/// Decrease the reference count on this object.
+/// The object will be descroyed when the last reference is removed.
+/// Does nothing if [object] is [NULL].
 void ut_object_unref(UtObject *object);
 
+/// Add a weak reference on this object and set [object_ref] to be this object.
+/// When this object is dereferenced, [object_ref] will be set to [NULL].
 void ut_object_weak_ref(UtObject *object, UtObject **object_ref);
 
+/// Removes a weak reference from [object_ref].
 void ut_object_weak_unref(UtObject **object_ref);
 
+/// Sets [object] to be a reference to [value]. If [object] already contained a
+/// value, it is unreferenced.
+///
+/// !arg-type value UtObject
 static inline void ut_object_set(UtObject **object, UtObject *value) {
   ut_object_unref(*object);
   *object = ut_object_ref(value);
 }
 
+/// Clears [object] to NULL. If [object] already contained a value, it is
+/// unreferenced.
 static inline void ut_object_clear(UtObject **object) {
   ut_object_unref(*object);
   *object = NULL;
