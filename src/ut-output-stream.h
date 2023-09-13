fc4a55d6724abe813fe95c65a83d74941afa628a
Robert Ancell <robert.ancell@gmail.com>
2023-09-13 21:11:37 +1200

    Document UtOutputStream

--- src/ut-output-stream.h
+++ src/ut-output-stream.h
@@ -13,8 +13,15 @@ typedef struct {
 
 extern int ut_output_stream_id;
 
+/// Write [data] to this stream.
+///
+/// !arg-type data UtList
 void ut_output_stream_write(UtObject *object, UtObject *data);
 
+/// Write [data] to this stream.
+/// [callback] is called when this data has been processed.
+///
+/// !arg-type data UtList
 void ut_output_stream_write_full(UtObject *object, UtObject *data,
                                  UtObject *callback_object,
                                  UtOutputStreamCallback callback);
