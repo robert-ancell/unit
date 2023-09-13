cca065e2f72635c5ccefad8788f525132edaf573
Robert Ancell <robert.ancell@gmail.com>
2023-09-13 21:31:24 +1200

    Document UtBufferedInputStream

--- src/ut-buffered-input-stream.h
+++ src/ut-buffered-input-stream.h
@@ -5,8 +5,17 @@
 
 #pragma once
 
+/// Create a new input stream where unread data is buffered.
+///
+/// !return-ref
+/// !return-type UtBufferedInputStream
 UtObject *ut_buffered_input_stream_new();
 
+/// Write [data] to this stream.
+/// If [complete] is set to true then no more data will be written after this
+/// call.
+///
+/// !arg-type data UtList
 void ut_buffered_input_stream_write(UtObject *object, UtObject *data,
                                     bool complete);
 
