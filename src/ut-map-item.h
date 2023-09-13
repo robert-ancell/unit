fcd4adf3d2a87e5f5b25ffc6721ac3b4b702c158
Robert Ancell <robert.ancell@gmail.com>
2023-09-13 21:26:17 +1200

    Document UtMapItem

--- src/ut-map-item.h
+++ src/ut-map-item.h
@@ -11,8 +11,14 @@ typedef struct {
 
 extern int ut_map_item_id;
 
+/// Gets the key this item is using.
+///
+/// !return-type UtObject
 UtObject *ut_map_item_get_key(UtObject *object);
 
+/// Gets the value this item is using.
+///
+/// !return-type UtObject
 UtObject *ut_map_item_get_value(UtObject *object);
 
 /// Returns [true] if [object] is a [UtMapItem].
